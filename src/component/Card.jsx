4ca0544c2a10dd7d35d3c785155282842560185e
ankitinfograins <ankitbanetiya@infograins.com>
2024-01-09 18:28:33 +0530

    first commit

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,100 +1,97 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react';
 import "slick-carousel/slick/slick.css";
-import data from '../data';
 import "slick-carousel/slick/slick-theme.css";
+import data from '../data';
 import '../component/CardCss.css';
-const Card = () => {
-  const [selected, setSelected] = useState(data[0])
-  // const setBottle =(item)=>{
-  //  setSelected({...data,img:item.img,backGroundColor:item.backGroundColor,prodName:item.prodName,price:item.price})
-  //  console.log(product)
-  // }
-  const setBottle = (item) => {
-    setSelected(selected[item])
-  }
-  //   const slideItem = [
-  //     { 
-  //       id: "1",
-  //       img: bottle1,
-  //       backGroundColor: `#f7f7f7`,
-  //       prodName:`Black / Onyx`,
-  //       price:`89`
-  //     },
-  //     {
-  //       id: "2",
-  //       img: bottle2,
-  //       backGroundColor: `#f8f8f6`,
-  //       prodName:`White / Dune`,
-  //       price:`120`
-  //     },
-  //     {
-  //       id: "3",
-  //       img: bottle3,
-  //       backGroundColor: `#f6f8f8`,
-  //       prodName:`White / Pebble`,
-  //       price:`200`
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
-  //     },
-  //     {
-  //       id: "4",
-  //       img: bottle4,
-  //       backGroundColor: `#f8faf5`,
-  //       prodName:`Black / Pine`,
-  //       price:`79`
-  //     },
-  //     {
-  //       id: "5",
-  //       img: bottle5,
-  //       backGroundColor: `#fef1f0`,
-  //       prodName:`White / Coral`,
-  //       price:`99`
-  //     }
-  //   ]
-  // const setBottle =(item)=>{
-  //  setProduct({...product,img:item.img,backGroundColor:item.backGroundColor,prodName:item.prodName,price:item.price})
-  //  console.log(product)
-  // }
-  //   const settings = {
-  //     // dots: true,
-  //     infinite: true,
-  //     speed: 500,
-  //     slidesToShow: 4,
-  //     slidesToScroll: 1
-  //   };
+const Card = () => {
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
+
+  const [selected, setSelected] = useState(data.map((item) => ({
+    prodName: item.subImg[0].prodName,
+    prodCol: item.subImg[0].prodCol,
+    price: item.subImg[0].price,
+    img: item.subImg[0].img,
+    bgColor: item.subImg[0].bgColor,
+  })));
+
+  const setBottle = (item, index) => {
+    const updatedProducts = [...selected];
+    updatedProducts[index] = {
+      prodName: item.prodName,
+      prodCol: item.prodCol,
+      price: item.price,
+      img: item.img,
+      bgColor: item.bgColor,
+    };
+    setSelected(updatedProducts);
+  };
+
   return (
-    <section className='sec vh-100' >
-      <div className='container'>
-        {data.map((product) => {
-          return (
-            <div className='card' key={product.id} >
+    <section className='sec vh-100'>
+         
+          
+     
+      
+           <div className='container'>
+            <Carousel
+          responsive={responsive}
+     
+        >
+      {data.map((product, index) => (
+         
+            <div className='card' key={product.id} style={{ backgroundColor: selected[index].bgColor }}>
               <div className='proMedia'>
-                <img src={require(`../assets/images/${selected.subImg[0].img}`)} alt='bottle1' />
+                <img src={require(`../assets/images/${selected[index].img}`)}  />
               </div>
               <div className='shopCard-content'>
-                <div className='productTitle '>
-                  <div className=' d-flex center'>
-                    <h3 >{selected.subImg[0].prodName}</h3>
-                    <span >{selected.subImg[0].prodCol}</span>
+                <div className='productTitle'>
+                  <div className='d-flex center'>
+                    <h3>{selected[index].prodName}</h3>
+                    <span>{selected[index].prodCol}</span>
                   </div>
                 </div>
                 <div className='prodSwiper'>
                   {product.subImg.map((item) => (
                     <div className='productList' key={item.id}>
-                      <button onClick={setBottle(data[item])} >
+                      <button onClick={() => setBottle(item, index)}>
                         <img src={require(`../assets/images/${item.img}`)} />
                       </button>
                     </div>
                   ))}
                 </div>
                 <div className='prodPrice'>
-                  <span>From ${selected.subImg[0].price}</span>
+                  <span>From ${selected[index].price}</span>
                 </div>
               </div>
             </div>
-          )
-        })}
-      </div>
+          
+          ))}      </Carousel></div>
+          
+   
+  
+
     </section>
-  )
-}
+  );
+};
+
 export default Card;
